0f58baa2daabf9dd9f824e9600604114bd1b723f
nparkes05 <72634133+nparkes05@users.noreply.github.com>
2021-05-03 11:30:35 -0400

    Add files via upload
    Second commit

---
+++ JSONdatabase.js
@@ -0,0 +1,72 @@
+var Workouts = [
+    {
+        "Workout": "Standard Pushup",
+        "Type": "Body",
+        "MuscleGroup": "Chest"
+    },
+    {
+        "Workout": "Bench Press",
+        "Type": "Gym",
+        "MuscleGroup": "Chest"
+    },
+    {
+        "Workout": "Body Weight Dips",
+        "Type": "Body",
+        "MuscleGroup": "Arm"
+    },
+    {
+        "Workout": "Body Weight Curls",
+        "Type": "Gym",
+        "MuscleGroup": "Arm"
+    },
+    {
+        "Workout": "Box Jumps",
+        "Type": "Body",
+        "MuscleGroup": "Leg"
+    },
+    {
+        "Workout": "Leg Press",
+        "Type": "Gym",
+        "MuscleGroup": "Leg"
+    },
+    {
+        "Workout": "Sit-Ups",
+        "Type": "Both",
+        "MuscleGroup": "Core"
+    },
+    {
+        "Workout": "Russian Twists",
+        "Type": "Both",
+        "MuscleGroup": "Core"
+    },
+    {
+        "Workout": "Pike Push-Ups",
+        "Type": "Body",
+        "MuscleGroup": "Shoulder"
+    },
+    {
+        "Workout": "Shoulder Press",
+        "Type": "Gym",
+        "MuscleGroup": "Shoulder"
+    },
+    {
+        "Workout": "Lawnmowers",
+        "Type": "Body",
+        "MuscleGroup": "Back"
+    },
+    {
+        "Workout": "Pull-Ups",
+        "Type": "Gym",
+        "MuscleGroup": "Back"
+    },
+    {
+        "Workout": "Child-Pose",
+        "Type": "Body",
+        "MuscleGroup": "Stretch"
+    },
+    {
+        "Workout": "Toe-Touches",
+        "Type": "Body",
+        "MuscleGroup": "Stretch"
+    }
+];
